07dec36d11e8ae2f78bc6957082833bfe8f3865a
Valbod-I-am-a-Swiftie <valbod@mail.ru>
2019-12-12 00:19:36 +0300

    Homework 6: minor fix

--- src/test/java/hw6/RunTests.java
+++ src/test/java/hw6/RunTests.java
@@ -1,4 +1,4 @@
-package hw6.ex2;
+package hw6;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
--- src/test/java/hw6/ex1/RunTests.java
+++
@@ -1,8 +0,0 @@
-package hw6.ex1;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(glue = "hw6")
-public class RunTests extends AbstractTestNGCucumberTests {
-}
